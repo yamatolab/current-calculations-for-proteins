e8baf07bb92f9596b9349eb7312a892de1efe049
Olivier Laprévote <olaprevote@protonmail.com>
2019-03-13 15:56:29 +0900

    Deleted test file. Moved the content of CURP-v1.1/ in the main directory.

--- CURP-v1.1/test/dynamics/vverlet/run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash -e
-
-rm -rf outdata
-mkdir -p outdata
-
-# calculate energy flux
-time  $CURP_HOME/bin/curp  run.cfg > log
-
--- CURP-v1.1/test/energy-flux/shell-parallel/clean.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-rm -rf  *.log *.run.cfg *.outdata
