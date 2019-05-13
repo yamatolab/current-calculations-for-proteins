6e3898ecbe15b8c48d002310bacd9f95153c20b7
Olivier Laprévote <olaprevote@protonmail.com>
2019-05-13 17:48:48 +0900

    Enhance setup.py
    setup.py now installs requirements, create and install scripts and
    is more silencious (f2py set in silent mode).

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 import setuptools
-from numpy.distutils.core import setup, Extension
+from numpy.distutils.core import setup
+from numpy.distutils.misc_util import Configuration
 import sys
 import os
 import fnmatch
 
-ext = Extension("lib_current",
-                sources=["src/current/lib_current.f90"])
-
-def ext_modules():
+def ext_modules(config, _dir):
     """Fetch f90 files in src and create automatically an extension"""
     curp_dir = sys.path[0]
     ext_modules = []
     os.chdir(curp_dir)
-    _dir = "src"
     pattern = "*.f90"
     if os.path.isdir(_dir):
         for root, dirs, files in os.walk(_dir):
@@ -20,25 +17,24 @@ def ext_modules():
             for name in fs:
                 f90_file = os.path.join(root, name)
                 ext_name = os.path.splitext(f90_file)[0].replace("/",".")
-                ext_modules.append(Extension(ext_name,
-                                             [f90_file],
-                                             # f2py_options*['--quiet']
-                                             ))
-    return ext_modules
-
-
+                config.add_extension(ext_name,
+                                     [f90_file],
+                                     f2py_options=['--quiet']
+                                     )
 
 def run_setup():
-    ext_modules()
+    config = Configuration(None, '', '') 
+    ext_modules(config, "src")
+    ext_modules(config, "script")
     setup(
         name="CURP",
-        version="1.2",
+        version="1.2.dev",
         author="Yamato's Lab",
         author_email="yamato@nagoya-u.jp",
         description="Current calculations between protein residues from MD trajectory",
         url=("https://gitlab.com/yamato97/current-calculations-for-proteins"),
         classifiers=[
-            "Development Status :: ",
+            "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Science/Research",
             "Operating System :: MacOS",
             "Operating System :: POSIX :: Linux",
@@ -49,8 +45,26 @@ def run_setup():
             "Topic :: Scientific/Engineering :: Bio-Informatics",
             "Topic :: Scientific/Engineering :: Physics"
             ],
-        install_requires=['numpy'],
-        ext_modules=ext_modules()
+
+        python_requires=">=2.7, <3.0",
+
+        install_requires=["numpy>=1.11.2",
+                          "nose",
+                          "mpi4py>=2.0",
+                          "benchmarker",
+                          "setproctitle",
+                          "epydoc",
+                          "pygraphviz",
+                          "netCDF4>=1.2.4"],
+        entry_points={
+            "console_scripts": [
+                "curp = src.curp:main",
+                "cal_tc = script.cal_tc:main",
+                "conv_trj = script.conv_trj:main",
+                "graph_een = script.graph_een:main"
+                ]
+            },
+        **config.todict()
         )
 
 if __name__ == '__main__':
