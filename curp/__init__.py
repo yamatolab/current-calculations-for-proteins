778af82d266fa0e4237af7f52b6c19c2608ab2df
Olivier Laprévote <olaprevote@protonmail.com>
2019-05-20 19:21:10 +0900

    Fix import bug

--- curp/__init__.py
+++ curp/__init__.py
@@ -1,21 +1,20 @@
 # Getting main curp function.
 from .curp import curp
 
+# Writer
+from .parser.writer import Writer as TrjWriter
+
+import parallel
+if parallel.use_mpi:
+    ParallelProcessor = parallel.ParallelProcessor
+else:
+    ParallelProcessor = parallel.SequentialProcessor
+
 # Topology
-from.parser import get_tplprm_simple as get_tpl
+from .parser import get_tplprm_simple as get_tpl
 
 # Trajectory
 from .script.conv_trj import gen_trj
 
 # Get all the nice scripts
-import .script as script
-
-
-# writer
-from .parser.writer import Writer as TrjWriter
-
-import curp.parallel as parallel
-if parallel.use_mpi:
-    ParallelProcessor = parallel.ParallelProcessor
-else:
-    ParallelProcessor = parallel.SequentialProcessor
+import script
