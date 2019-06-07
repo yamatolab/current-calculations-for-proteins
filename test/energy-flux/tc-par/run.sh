dba3e4f3cd36191888433fafd1540da18e364b5f
O.Laprevote <olaprevote@protonmail.com>
2019-06-07 18:53:11 +0900

    Update tc-par test

--- test/energy-flux/tc-par/run.sh
+++ test/energy-flux/tc-par/run.sh
@@ -5,7 +5,7 @@ nproc=${1:-2}
 mkdir -p outdata
 rm -f outdata/*.dat*
 
-time mpirun -n $nproc  curp cal-tc \
+time mpirun -n $nproc  curp cal-tc --no-axes\
     --frame-range 1 50000 1 \
     --average-shift 1 --sample-number 0 \
     -o outdata/tc.dat -a outdata/acf.nc flux_grp.nc > log
