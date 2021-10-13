8b35bf4179505200ca9ea4f0a3099b394b6fefdc
Wataru Sugiura <38929316+flat35hd99@users.noreply.github.com>
2021-10-13 12:00:51 +0900

    Update core of acf and flux calculations (#18)
    * TSRC Protein Dynamics 2021 version
    
    * TSRC Protein Dynamics 2021 version modified
    
    Co-authored-by: yamato <yamato@nagoya-u.jp>

--- curp/script/lib_acf.f90
+++ curp/script/lib_acf.f90
@@ -12,13 +12,7 @@ subroutine cal_acf(acf, xss, nacf, first, last, interval, shift, &
    real(8) :: x2_sum_inv(ncom), acf_tmp(nacf, ncom)
    integer :: iacf, ifrm, ifrm_beg, nfrm_beg, icom, nsam
    logical :: use_norm
-
-   ! determine the default value of use_norm
-   if ( present(norm) ) then
-      use_norm = norm
-   else
-      use_norm = .false.
-   end if
+   real(8) :: a, norm_flg, b(ncom)
 
    ! determine the number of samples
    if ( nsample <= 0 ) then
@@ -29,36 +23,39 @@ subroutine cal_acf(acf, xss, nacf, first, last, interval, shift, &
       nfrm_beg = first + shift*(nsample-1)
    end if
 
+   ! determine the default value of use_norm
+   if ( present(norm) ) then
+      use_norm = norm
+      norm_flg = 1.0d0
+   else
+      use_norm = .false.
+      norm_flg = 0.0d0
+   end if
+ 
    ! << Calculate auto-correlation function >>
-   do ifrm_beg=first, nfrm_beg, shift
-
-      ! x**2 for norm
-      if (use_norm) then
-         x2_sum_inv(:) = 1.0d0/(xss(ifrm_beg,:)*xss(ifrm_beg,:))
-      else
-         x2_sum_inv(:) = 1.0d0
-      end if
-
-      ! calculate acf
-      acf_tmp(:,:) = 0.0d0
+   do icom=1, ncom
       do iacf=1, nacf
-         ifrm = ifrm_beg + (iacf-1)*interval
-         acf_tmp(iacf,:) = xss(ifrm_beg,:) * xss(ifrm,:)
+         a=0.0d0
+         b(icom)=0.0d0
+         do ifrm_beg=first, nfrm_beg, shift
+
+            ! if use_norm then x2_sum_inv = 1/J(0)J(0) else x2_sum_inv = 1.0d0
+            x2_sum_inv(icom) = 1.0d0/(xss(ifrm_beg,icom)*xss(ifrm_beg,icom))
+            x2_sum_inv(icom) = x2_sum_inv(icom) - 1.0d0
+            x2_sum_inv(icom) = (x2_sum_inv(icom) * norm_flg) + 1.0d0
+
+            ifrm = ifrm_beg + (iacf-1)*interval
+            a = a + ( ( xss(ifrm_beg,icom) * xss(ifrm,icom) ) * x2_sum_inv(icom) )
+            b(icom) = b(icom) + (1.0d0/x2_sum_inv(icom))
+
+         end do
+         acf(iacf,icom) = a
       end do
-
-      ! sum up acf by the number of samples
-      do icom=1, ncom
-         acf(:,icom) = acf(:,icom) + acf_tmp(:,icom)*x2_sum_inv(icom)
-      end do
-
-      ! integral acf
-      ! print*, '#lib ', sum(acf_tmp(:,1))
-
    end do ! samples of one file data
 
    nsam = (nfrm_beg-first)/shift + 1
    ! print*, first, nfrm_beg, nsam
    acf(:,:) = acf(:,:) / real(nsam)
-
+   b(:) = b(:) / real(nsam)
 end subroutine
 
