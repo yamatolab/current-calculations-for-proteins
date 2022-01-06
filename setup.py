3705c4af2877fc9d4be067bd8ce7b3ea8bca0c15
Wataru Sugiura <flat35hd99@gmail.com>
2022-01-06 17:02:44 +0900

    Update version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def run_setup():
     config.add_data_files(os.path.join("curp", "LICENSE-short.txt"))
     setup(
         name="Curp",
-        version="1.3",
+        version="1.3.1",
         author="Yamato's Lab",
         author_email="yamato@nagoya-u.jp",
         description="Inter-residue Current calculation in Proteins from MD \
