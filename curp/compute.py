18e79129e7ae117670597a9489bdb704f733c8bb
vantelindome <arita.yoichi.w1@s.mail.nagoya-u.ac.jp>
2024-12-06 11:08:21 +0900

    modified reference variable of length of gpair_table

--- curp/compute.py
+++ curp/compute.py
@@ -298,7 +298,7 @@ def init_current(setting, par):
     # Decide Writer.
     t_0 = time.time()
     
-    logger.info("length of gpair_table: {}".format(len(gpair_table)))
+    logger.info("length of gpair_table: {}".format(len(interact_table)))
 
     if par.is_root():
         from curp.current.writer import get_writer
