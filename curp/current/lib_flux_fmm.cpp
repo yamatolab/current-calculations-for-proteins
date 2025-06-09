e8ef597e4396b99f7ab976e1d825f6b6d2e251b6
vantelindome <arita.yoichi.w1@s.mail.nagoya-u.ac.jp>
2025-06-09 13:21:44 +0900

    changed vij -> vi

--- curp/current/lib_flux_fmm.cpp
+++ curp/current/lib_flux_fmm.cpp
@@ -795,10 +795,10 @@ public:
 
     };
 
-    void cal_eflux_atomwise(Vector3d fij, Vector3d vij, Vector3d rij, int igrp, int jgrp){
+    void cal_eflux_atomwise(Vector3d fij, Vector3d vi, Vector3d rij, int igrp, int jgrp){
 
-        double e_ij = fij.dot(vij) * 0.5;
-        
+        double e_ij = fij.dot(vi) * 0.5;
+
         if (igrp != jgrp){
             eflux_ij(igrp-1, jgrp-1) += e_ij;
             eflux_ij(jgrp-1, igrp-1) += -e_ij;
