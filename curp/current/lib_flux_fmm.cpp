f9760b20bb9f50c87ecc415fe9d8e382283576a5
vantelindome <arita.yoichi.w1@s.mail.nagoya-u.ac.jp>
2025-06-20 15:01:25 +0900

    fixed intra heat flux addition

--- curp/current/lib_flux_fmm.cpp
+++ curp/current/lib_flux_fmm.cpp
@@ -774,7 +774,7 @@ public:
             hflux_ij[jgrp-1][igrp-1] += h_ij;
         }
         else {
-            hflux_ij[igrp-1][jgrp-1] += 0.5 * h_ij;
+            hflux_ij[igrp-1][jgrp-1] += h_ij;
         }
         count_atom += 1;
     };
